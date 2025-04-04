b7d90f79fcbfc0469584f4f9c432afc04ea37d95
DevCristianGonzalez <cristianjussepalarcongonzalez@gmail.com>
2025-04-03 20:54:01 -0500

    prueba error 404

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ self.addEventListener('install', (event) => {
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+        return response || fetch(event.request).catch(() => caches.match('/index.html'));
       })
     );
   });
